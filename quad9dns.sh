6825e21f8c49f574d8e2962b7f5b22bf25a7c7eb
Ben Koe <mail@benkoe.in>
2023-04-24 19:47:38 +0200

    Update quad9dns.sh
    Add automatic switch to quad9dns connection profile

--- quad9dns.sh
+++ quad9dns.sh
@@ -2,3 +2,4 @@ cp quad9dns.nmconnection /etc/NetworkManager/system-connections/
 chown root:root /etc/NetworkManager/system-connections/quad9dns.nmconnection
 chmod 600 /etc/NetworkManager/system-connections/quad9dns.nmconnection
 nmcli connection reload
+nmcli connection up quad9dns
