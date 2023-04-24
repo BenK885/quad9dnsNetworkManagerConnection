be5aa0fac096471a46a752026897b3bb4fda4fb1
Ben Koe <mail@benkoe.in>
2023-04-24 19:36:18 +0200

    Add files via upload

---
+++ quad9dns.sh
@@ -0,0 +1,4 @@
+cp quad9dns.nmconnection /etc/NetworkManager/system-connections/
+chown root:root /etc/NetworkManager/system-connections/quad9dns.nmconnection
+chmod 600 /etc/NetworkManager/system-connections/quad9dns.nmconnection
+nmcli connection reload
