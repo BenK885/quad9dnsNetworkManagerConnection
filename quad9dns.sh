7674bfd4310cf6b6d7e5db13b872f206c8166b81
Ben Koe <mail@benkoe.in>
2023-04-24 20:41:15 +0200

    Update quad9dns.sh

--- quad9dns.sh
+++ quad9dns.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cp quad9dns.nmconnection /etc/NetworkManager/system-connections/
 chown root:root /etc/NetworkManager/system-connections/quad9dns.nmconnection
 chmod 600 /etc/NetworkManager/system-connections/quad9dns.nmconnection
